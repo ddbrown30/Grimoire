cd38c703e63c91808f91d30fa46d29e26af816d8
Dan Brown <danbrown@backtablestudios.com>
2020-07-17 18:18:41 -0400

    Fixed AddToGrimoire==false tokens not creating their helper tokens

--- Assets/Scripts/GrimoireManager.cs
+++ Assets/Scripts/GrimoireManager.cs
@@ -78,7 +78,6 @@ public class GrimoireManager : MonoBehaviour, IPointerClickHandler
         }
         else
         {
-            grimoireToken.gameObject.SetActive(false);
             HiddenTokens.Add(grimoireToken);
         }
 
--- Assets/Scripts/GrimoireToken.cs
+++ Assets/Scripts/GrimoireToken.cs
@@ -61,6 +61,11 @@ public class GrimoireToken : MonoBehaviour, IBeginDragHandler, IDragHandler, IDr
         {
             CreateAlignmentToken();
         }
+
+        if(RoleData.AddToGrimoire == false && IsBluffToken == false)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     void Update()
